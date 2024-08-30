45f5d7445a98e3594729d634f42f8ee818e9ec8c
TienNhan98 <tiengiahan@gmail.com>
2024-08-30 16:24:36 +0700

    DOne

--- utils/renderItem.js
+++ utils/renderItem.js
@@ -1,18 +1,16 @@
 export function renderItems(type, items) {
   const container = document.getElementById("tab-content-container");
-  container.innerHTML = "";
+  container.innerHTML = ""; // Xóa nội dung hiện tại
 
   const filteredItems = items.filter((item) => item.type === type);
 
   filteredItems.forEach((item) => {
     const itemHtml = `
-      <div class="col-md-4 mb-4">
-        <div class="card">
-          <img src="${item.imgSrc_jpg}" alt="${item.name}" class="card-img-top">
-          <div class="card-body text-center">
-            <h5 class="card-title">${item.name}</h5>
-            <button class="btn btn-primary try-btn" onclick="tryOnItem('${item.imgSrc_png}', '${item.type}')">Thử đồ</button>
-          </div>
+      <div class="card">
+        <img src="${item.imgSrc_jpg}" alt="${item.name}" class="card-img-top">
+        <div class="card-body text-center">
+          <h5 class="card-title">${item.name}</h5>
+          <button class="btn btn-primary try-btn" onclick="tryOnItem('${item.imgSrc_png}', '${item.type}')">Thử đồ</button>
         </div>
       </div>
     `;
